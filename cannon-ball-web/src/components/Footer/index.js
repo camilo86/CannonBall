d5e7fdd66879328c1425e8ad75fa0ac9372b9bd2
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2019-03-31 03:21:03 -0400

    add footer

---
+++ cannon-ball-web/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Pane, Text, Icon } from 'evergreen-ui';
+
+const Footer = () => (
+  <Pane width="100%" display="flex" justifyContent="center" padding={16} background="#084B8A">
+    <Text color="white">Made with <Icon icon="heart" size={10} /> in Rochester, NY.</Text>
+  </Pane>
+);
+
+export default Footer;
--- cannon-ball-web/src/containers/HomePage/index.js
+++ cannon-ball-web/src/containers/HomePage/index.js
@@ -5,6 +5,7 @@ import Navbar from '../../components/Navbar';
 import FileChooser from '../../components/FileChooser';
 import HowItWorks from '../../components/HowItWorks';
 import Faq from '../../components/Faq';
+import Footer from '../../components/Footer';
 
 class HomePage extends Component {
   render() {
@@ -14,6 +15,7 @@ class HomePage extends Component {
         <FileChooser />
         <HowItWorks />
         <Faq />
+        <Footer />
       </Pane>
     );
   }
