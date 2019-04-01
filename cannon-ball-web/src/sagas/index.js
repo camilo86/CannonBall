2f16d4e23d9a010107757aedab9e720f56e97154
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2019-03-31 20:00:46 -0400

    add saga integration

---
+++ cannon-ball-web/src/sagas/index.js
@@ -0,0 +1,9 @@
+import { all } from 'redux-saga/effects';
+
+import application from './application';
+
+export default function* rootSaga() {
+  yield all([
+    ...application,
+  ]);
+}
--- cannon-ball-web/src/store/index.js
+++ cannon-ball-web/src/store/index.js
@@ -1,8 +1,15 @@
 import { configureStore } from 'redux-starter-kit';
+import createSagaMiddleware from 'redux-saga';
+import sagas from '../sagas';
 import rootReducer from '../reducers';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [sagaMiddleware]
 });
+
+sagaMiddleware.run(sagas);
 
 export default store;
